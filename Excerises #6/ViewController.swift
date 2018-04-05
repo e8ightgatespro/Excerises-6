64aa02abea2799f8b611c907408f580d3ff12b5b
e8ightgatespro <garrettvoss4@gmail.com>
2018-04-05 13:46:18 -0500

    Initial Commit

---
+++ Excerises #6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Excerises #6
+//
+//  Created by Voss, Garrett on 4/5/18.
+//  Copyright © 2018 Voss, Garrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
